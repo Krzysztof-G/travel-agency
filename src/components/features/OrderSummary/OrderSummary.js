0441d5866f78849eabd2264426129052ed80c165
Krzysztof Gumuła <k.gumula@icloud.com>
2020-05-13 19:43:08 +0200

    add trip options & price

---
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from '../OrderSummary/OrderSummary.scss';
+import PropTypes from 'prop-types';
+import {calculateTotal} from '../../../utils/calculateTotal';
+import {formatPrice} from '../../../utils/formatPrice';
+
+
+const OrderSummary = ({tripCost, options}) => {
+
+  const price = calculateTotal(formatPrice(tripCost), options);
+
+  return(
+    <h2 className={styles.component}>
+    Total:
+      <strong>
+        ${price}
+
+      </strong>
+    </h2>
+  );
+};
+
+OrderSummary.propTypes = {
+  tripCost: PropTypes.string,
+  options: PropTypes.object,
+};
+
+export default OrderSummary;
