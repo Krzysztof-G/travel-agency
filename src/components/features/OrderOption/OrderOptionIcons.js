e711bbc637bf76801002cb8f725ed521bace5b1f
Krzysztof Gumuła <k.gumula@icloud.com>
2020-05-14 22:25:12 +0200

    add OrderOptionIcons

--- src/components/features/OrderOption/OrderOptionIcons.js
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -1,14 +1,43 @@
 import React from 'react';
+import styles from '../OrderOption/OrderOption.scss';
+import {formatPrice} from '../../../utils/formatPrice';
+import PropTypes from 'prop-types';
+import Icon from '../../common/Icon/Icon';
 
 
 
-const OrderOptionIcons = () => {
+const OrderOptionIcons = ({values, required, currentValue, setOptionValue}) => (
+  <div className={styles.icon}>
+    {required ? '' : (
+      <div
+        className={styles.icon}
+        value={currentValue}
+        onChange={() => setOptionValue('')}
+      >
+        <Icon name={'times-circle'} />
+        none
+      </div>
+    )}
 
-  return (
-    <div>
-      OrderOptionIcons
-    </div>
-  );
+    {values.map(value => (
+      <div
+        className={currentValue === value.id ? styles.iconActive : styles.icon}
+        key={value.id}
+        value={value.id}
+        onClick={() => (setOptionValue(value.id))}
+      >
+        <Icon name={value.id} />
+        {value.name} ({formatPrice(value.price)})
+      </div>
+    ))}
+  </div>
+);
+
+OrderOptionIcons.propTypes = {
+  values: PropTypes.array,
+  required: PropTypes.bool,
+  currentValue: PropTypes.node,
+  setOptionValue: PropTypes.func,
 };
 
 
