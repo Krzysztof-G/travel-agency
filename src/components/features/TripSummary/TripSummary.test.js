be43ab6fdfad5140953acdd6932ba30e59882be5
Krzysztof Gumuła <k.gumula@icloud.com>
2020-05-19 19:16:43 +0200

    improve TripSummary test

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -9,5 +9,12 @@ describe('Component TripSummary', () => {
     const component = shallow(<TripSummary id={expectedId} tags={[]} />);
     expect(component.find('.link').prop('to')).toEqual(`/trip/${expectedId}`);
   });
-});
+  it('check correct src and alt of image', () => {
+    const expectedSrc = 'image.jpg';
+    const expectedAlt = 'photo';
 
+    const component = shallow(<TripSummary image={expectedSrc} name={expectedAlt} tags={[]} />);
+
+    expect(component.find('.img').prop('src')).toEqual(`/trip/${expectedSrc}`);
+  });
+});
