e293c56b987a3fc53d85d487b0c36dff3d7fd1b2
Krzysztof Gumuła <k.gumula@icloud.com>
2020-05-28 14:48:19 +0200

    add formatTime

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,1 +1,16 @@
-export const formatTime = () => 'formatted time';
+export const formatTime = (time) => {
+  if(typeof time == 'undefined' || null || typeof time !== 'number' || time < 0){
+    return null; }
+
+  else {
+    const seconds = Math.floor(time % 60);
+    const minutes = Math.floor((time / 60) % 60);
+    const hours = Math.floor((time / 3600) % 60);
+
+    const correctSeconds = seconds < 10 ? `0${seconds}` : seconds;
+    const correctMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    const correctHours = hours < 10 ? `0${hours}` : hours;
+
+    return (`${correctHours}:${correctMinutes}:${correctSeconds}`);
+  }
+};
